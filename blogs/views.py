3cbbdfd9ab2b06323caaaaaef84d12050e35e4bc
Hugo Pedraza Reyes <hpedraza@farmaciasdirect.com>
2025-07-14 12:01:10 +0200

    subiendo a Github

---
+++ blogs/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import redirect, get_object_or_404,render
+from django.http import HttpResponse
+
+from blogs.models import Blog, Category
+
+# Create your views here.
+def posts_by_category(request, category_id):
+    posts = Blog.objects.filter(status='Published', category=category_id)
+
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect ('home')
+    
+
+    context = {
+        
+        'posts' : posts,
+        'category' : category,
+
+        
+    }
+    
+    return render(request, 'posts_by_category.html' , context)
+    
