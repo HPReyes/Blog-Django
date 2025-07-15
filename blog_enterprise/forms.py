0bf894fbfa74aa7e197ecf1be65e034b79f74ede
Hugo Pedraza Reyes <hpedraza@farmaciasdirect.com>
2025-07-15 14:51:01 +0200

    editar y eliminar

--- blog_enterprise/forms.py
+++ blog_enterprise/forms.py
@@ -6,4 +6,13 @@ class RegistrationForm(UserCreationForm):
         model = User
         fields = ('email', 'username', 'password1', 'password2')
 
-
+    def save(self, commit = True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.is_staff = True
+        
+        if commit:
+            user.save()
+        
+        return user
+        
--- blog_enterprise/views.py
+++ blog_enterprise/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 from .forms import RegistrationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
+from django.contrib.auth.models import Group
 
 from blogs.models import Blog, Category
 
@@ -22,7 +23,10 @@ def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            group = Group.objects.get(name='blog_group')
+            group.user_set.add(user)
+            
             return redirect('register')
     else:
         form = RegistrationForm()
@@ -33,10 +37,12 @@ def register(request):
     
     return render(request, 'register.html', context)
 
+
 def login(request):
     if request.method == 'POST':
         
         form = AuthenticationForm(request, request.POST)
+        
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
@@ -44,8 +50,11 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-            return redirect('home')
-            
+                return redirect('home')
+    
+    else:
+        form = AuthenticationForm()  # ← aquí formulario para GET
+        
                
     
     context = {
